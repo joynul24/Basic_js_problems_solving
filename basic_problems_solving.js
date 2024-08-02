3d51b24752cf2f7e938de817c04b1ed3997fc450
joynul24 <joynula919@gmail.com>
2024-08-03 01:12:41 +0600

    added third number task

--- basic_problems_solving.js
+++ basic_problems_solving.js
@@ -37,3 +37,31 @@ function checkName(name) {
 // console.log(checkName('RAFEE'));
 // console.log(checkName(-2)); // invalid dekhabe
 
+
+
+
+
+
+// ত োমাকে deleteInvalids() নামে এমন একটা ফাংশন ক্রি য়ে ট করতে হবে যা ইনপুট হিসেবে একটি Array
+// নেবে । ফাংশন টি আরে কটি Array রি টার্ন করবে যে খানে ভ্যালুগুল ো হবে শুধুনাম্বার । অর্থাৎ Array এর যে সকল
+// elements নাম্বার না , তাদে র ডি লি ট করে শুধুমাত্র নাম্বার গুল ো কে Array আকারে রিটার্ন করতে হবে ।
+// তুমি যদি এই ফাংশনে এই [ NaN, 1,12,0 ,-1 , undefined ] Array টি পাস কর ো কর ো তাহলে সে ত োমাকে
+// রি টার্ন করবে [ 1 , 12 , 0 , -1 ]
+
+
+function deleteInvalids(array) {
+    if (Array.isArray(array) === false) {
+        return 'Error: please provide an array';
+    }
+
+    const numbersArray = [];
+    for (const item of array) {
+        if (typeof item === 'number') {
+            numbersArray.push(item);
+        }
+    }
+    return numbersArray;
+}
+
+const mixedArray = [1, 'hello', 3.14, true, 42, 'world', null, undefined, 7];
+console.log(deleteInvalids(mixedArray));
